70959cb70c08d6b8d9aee058bd8c5a3e6fcd7bc0
Neil Francis Nahid <neilnahid@remotodojo.com>
2021-09-02 16:45:57 +0800

    added pub sub implementation

--- src/index.js
+++ src/index.js
@@ -1,24 +1,39 @@
 const EventBusFactory = () => {
-    const listeners = {};
+  const listeners = {};
 
-    const fire = (event, payload) => {
-        // publish an event listener
-    };
-
-    const listen = (event, listener) => {
-        // create an event listener
-    };
-
-    const unsubscribe = (event, removeListener) => {
-        // unsubscribe a listener from event
-    };
-
-    return {
-        fire,
-        listen,
-        unsubscribe
+  const fire = (event, payload) => {
+    // publish an event listener
+    let result;
+    if (listeners[event])
+      result = listeners[event].map((listener) => listener(payload));
+    return result;
+  };
+  const listen = (event, listener) => {
+    // create an event listener
+    if (
+      event &&
+      listener &&
+      typeof listener === "function" &&
+      typeof event === "string"
+    ) {
+      if (Array.isArray(listeners[event]))
+        listeners[event] = [...listeners[event], listener];
+      else listeners[event] = [listener];
     }
-}
+  };
 
+  const unsubscribe = (event, removeListener) => {
+    // unsubscribe a listener from event
+    if (listeners[event] && removeListener) {
+      const listenerIndex = listeners[event].indexOf(removeListener);
+      listeners[event].splice(listenerIndex, 1);
+    }
+  };
 
-module.exports = EventBusFactory
+  return {
+    fire,
+    listen,
+    unsubscribe,
+  };
+};
+module.exports = EventBusFactory;
--- src/index.test.js
+++ src/index.test.js
@@ -1,29 +1,89 @@
-const EventBusFactory = require('./index')
+const EventBusFactory = require("./index");
 
 test("Test a published message received", () => {
-    const EventBus = EventBusFactory();
+  const EventBus = EventBusFactory();
 
-    EventBus.listen('new-message', payload => `The message is "${payload}"`);
+  EventBus.listen("new-message", (payload) => `The message is "${payload}"`);
 
-    const message = "You are awesome!";
+  const message = "You are awesome!";
 
-    expect(EventBus.fire('new-message', message)[0]).toBe(`The message is "${message}"`);
+  expect(EventBus.fire("new-message", message)[0]).toBe(
+    `The message is "${message}"`
+  );
 });
 
 test("Test another event subscriber", () => {
-    let EventBus = EventBusFactory();
+  const EventBus = EventBusFactory();
 
-    EventBus.listen('another-event', () => null);
+  EventBus.listen("another-event", () => null);
 
-    expect(EventBus.fire('another-event', 'null')).toStrictEqual([null]);
+  expect(EventBus.fire("another-event", "null")).toStrictEqual([null]);
 });
 
 test("Test unsubscribe from event", () => {
-    let EventBus = EventBusFactory();
-    let listener = payload => payload;
+  const EventBus = EventBusFactory();
+  const listener = (payload) => payload;
 
-    EventBus.listen('new-event', listener);
-    EventBus.unsubscribe('new-event', listener);
+  EventBus.listen("new-event", listener);
+  EventBus.unsubscribe("new-event", listener);
 
-    expect(EventBus.fire('new-event', true)).toStrictEqual([]);
+  expect(EventBus.fire("new-event", true)).toStrictEqual([]);
+});
+test("Test multiple listeners to one event", () => {
+  const EventBus = EventBusFactory();
+  EventBus.listen(
+    "event",
+    (payload) => `hello I'm the 1st event, here's your payload ${payload}`
+  );
+  EventBus.listen(
+    "event",
+    (payload) => `I'm the 2nd event, fancy some payload?${payload}`
+  );
+  const payload = "Im the payload!";
+  expect(EventBus.fire("event", payload)).toEqual([
+    `hello I'm the 1st event, here's your payload ${payload}`,
+    `I'm the 2nd event, fancy some payload?${payload}`,
+  ]);
+});
+test("Test firing an event with no listener", () => {
+  const EventBus = EventBusFactory();
+  const payload = "Im the payload!";
+  expect(EventBus.fire("event", "im a payload")).toBe(undefined);
+});
+
+test("Test adding 2 listeners, then firing the event, unsubscribing 1 event then firing again", () => {
+  const EventBus = EventBusFactory();
+  const listener1 = (payload) =>
+    `hello I'm the 1st event, here's your payload ${payload}`;
+  const listener2 = (payload) =>
+    `I'm the 2nd event, fancy some payload?${payload}`;
+  const payload = "Im the payload!";
+  EventBus.listen("event", listener1);
+  EventBus.listen("event", listener2);
+  expect(EventBus.fire("event", payload)).toEqual([
+    `hello I'm the 1st event, here's your payload ${payload}`,
+    `I'm the 2nd event, fancy some payload?${payload}`,
+  ]);
+  EventBus.unsubscribe("event", listener2);
+  expect(EventBus.fire("event", payload)).toEqual([
+    `hello I'm the 1st event, here's your payload ${payload}`,
+  ]);
+});
+
+test("Test invalid arguments to listen function", () => {
+  const EventBus = EventBusFactory();
+  EventBus.listen(0, () => null); // event name
+  EventBus.listen("test", "tests"); // invalid listener
+  EventBus.listen(); // arguments
+  expect(EventBus.fire("test")).toBe(undefined);
+});
+test("test unsubscribe multiple listeners", () => {
+  const EventBus = EventBusFactory();
+  const listener = (payload) => payload;
+  const listener2 = (payload) => payload;
+  EventBus.listen("event", listener);
+  EventBus.listen("event", listener2);
+  EventBus.unsubscribe("event", listener);
+  EventBus.unsubscribe("event", listener2);
+  expect(EventBus.fire("event", true)).toStrictEqual([]);
 });
